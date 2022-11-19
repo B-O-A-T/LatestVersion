754e0f5c5c16408ea6d858766a9946867aa440f2
Lorenzo Kearns <Kearnsl@my.erau.edu>
2022-11-18 23:02:46 -0700

    Al sensors ==goof; Cereal == good;  Vector Nav ==  good; Lorenzo out
    Cereal connection working, Vector Nav example working. This shit be slappin. someone else needs to fine tune these functions to vibe with the rest of the project.

--- Main.py
+++ Main.py
@@ -12,8 +12,10 @@ from CoursePlotter import CoursePlotter
 from utilities import Tools
 from Compass import Compass
 from SensorTranslator import Telemetry
+from VectorNav300 import VN300
 import time
 import csv
+from glob import *
 #****************************************************************#
 
 # class sailController():
@@ -29,33 +31,73 @@ import csv
 # begin main code:
 class main():
     def __init__(self):
-        self.timer1 = time.perf_counter()
-        self.CourseBoi = CoursePlotter(STARTING_POS, WAYPOINT1, self.compass.windRelNorth, MAX_PATH_DEVIATION)
-        self.compass = Compass(0, 0)
-        self.compass.vesselBearing = self.CourseBoi.set_path_type()
-        self.boatAngles, self.boatSpeeds = self.CourseBoi.define_speed(WIND_SPEED_KNOTTS)
-        self.desHeading = self.compass.vesselBearing
         self.Telem = Telemetry()
-        self.desHeading = self.CourseBoi.find_course(self.gpsLng, self.gpsLat, self.compass.vesselBearing)
-        devFromPathDist, self.directPath = self.CourseBoi.is_past_beat_max(STARTING_POS, WAYPOINT1, self.gpsLat, self.gpsLng)
+        self.SAK = Tools()
+        self.VN = VN300()
+        self.handshake_with_GC()
+        self.timer1 = time.perf_counter()
+        self.update_wind()
+        self.gpsLon, self.gpsLat = self.update_position()
+        self.trueVesselBearing = 0
+        self.STARTING_POS = self.gpsLon, self.gpsLat
+        self.read_waypoint_csv()
+        WAYPOINT1 = self.waypoints[0]
+        self.CourseBoi = CoursePlotter(self.STARTING_POS, WAYPOINT1, self.TWA, MAX_PATH_DEVIATION)
+        # # self.compass = Compass(0, 0)
+        self.desHeading = self.CourseBoi.find_course(self.gpsLon, self.gpsLat, self.trueVesselBearing) # We want to find a course 
+        devFromPathDist, self.directPath = self.CourseBoi.is_past_beat_max(self.STARTING_POS, WAYPOINT1, self.gpsLat, self.gpsLon)
+        self.run()
            
+    def read_waypoint_csv(self):
+        self.waypoints = []
+        with open('waypoints.csv') as file_obj:
+            reader_obj = csv.reader(file_obj)
+            for row in reader_obj:  
+                self.waypoints.append(row)        
+
+    def handshake_with_GC(self):
+        self.Telem.handshake_with_arduino()
+
     def update_wind(self):
-        windSpdApp = self.Telem.send_telem_request(Telem.telem.windSpeed)
-        windAngApp = self.Telem.send_telem_request(Telem.telem.apWind)
+        self.windSpdApp = self.Telem.send_telem_request(self.Telem.telem.windSpeed)
+        self.windAngApp = self.Telem.send_telem_request(self.Telem.telem.apWind)
         # Implement translation to true wind speed and direction
+        self.TWA = self.windAngApp
+        self.TWS = self.windSpdApp
+        print("Wind Speed: " + str(self.TWS))
+        print("Wind Angle: " + str(self.TWA))
+
 
     def update_bearing(self):
-        raise NotImplemented
+        self.trueVesselBearing = self.SAK.mod360(self.VN.get_bearing())
+        print("Vessel bearing: " + str(self.trueVesselBearing))
+        """
+            delete above values and 
+            add functionality to 
+            send and receive bearing
+            from vectorNav
+        """
 
     def update_position(self):
-        raise NotImplemented
+        x, y = self.VN.get_position()
+        """
+            delete above values and 
+            add functionality to 
+            send and receive lat and lon values 
+            from vectorNav
+        """
+        print("Longitude: " + str(x) + ", Lattitude: " + str(y))
+        return x, y
 
     def run(self):
         while True:
-            if(time.perf_counter() - self.timer1 > 10):
+            if(time.perf_counter() - self.timer1 > 1.5):
                 self.timer1 = time.perf_counter()
-                devFromPathDist, self.directPath = self.CourseBoi.is_past_beat_max(STARTING_POS, WAYPOINT1, self.gpsLat, self.gpsLng)
-                self.desHeading = self.CourseBoi.find_course(self.gpsLng, self.gpsLat, self.compass.vesselBearing)
+                devFromPathDist, self.directPath = self.CourseBoi.is_past_beat_max(self.STARTING_POS, WAYPOINT1, self.gpsLat, self.gpsLon)
+                self.desHeading = self.CourseBoi.find_course(self.gpsLon, self.gpsLat, self.trueVesselBearing)
+                self.update_wind()
+                self.update_bearing()
+                self.update_position()
                 # if(self.compass.vesselBearing != self.desHeading):
 
 if __name__ == "__main__":
